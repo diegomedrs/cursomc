f9ee17adf8a328197c80c8bfc021d72b389dc082
Diego Rodrigues <diegomedrs@gmail.com>
2021-03-09 15:52:59 -0400

    Produto e associacao muitos para muitos

--- src/main/java/com/nelioalves/mc/repositories/ProdutoRepository.java
+++ src/main/java/com/nelioalves/mc/repositories/ProdutoRepository.java
@@ -3,10 +3,10 @@ package com.nelioalves.mc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.nelioalves.mc.domain.Categoria;
+import com.nelioalves.mc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
 }
 
